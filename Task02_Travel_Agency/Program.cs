db8588a0c5240e7ce88046fcc4fd2235625f06b9
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-08 20:09:57 +0200

    Add files via upload

---
+++ Task02_Travel_Agency/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task02_Travel_Agency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfTickets = int.Parse(Console.ReadLine());
+            int budget = int.Parse(Console.ReadLine());
+            int priceForTicket = int.Parse(Console.ReadLine());
+
+            double sum = countOfTickets * priceForTicket;
+
+            if (sum <= budget)
+            {
+                Console.WriteLine($"You can sell your client {countOfTickets} tickets for the price of {(priceForTicket * countOfTickets)}$!");
+                Console.WriteLine($"Your client should become a change of {(budget - sum)}$!");
+            }
+            else
+            {
+                Console.WriteLine($"The budget of {budget}$ is not enough. Your client can't buy {countOfTickets} tickets with this budget!");
+            }
+            
+        }
+    }
+}
