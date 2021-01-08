f2421b61ad1b2172c30b1ade2c9d9d0a4af6fe9f
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-08 20:08:33 +0200

    Add files via upload

---
+++ Task01_Shoping_For_School/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task01_Shoping_For_School
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pencils = int.Parse(Console.ReadLine());
+            int feltTipPens = int.Parse(Console.ReadLine());
+            int sketchbooksForDrawing = int.Parse(Console.ReadLine());
+            int notebooks = int.Parse(Console.ReadLine());
+
+            double sum = (pencils * 4.75 + feltTipPens * 1.80 + sketchbooksForDrawing * 6.50 + notebooks * 2.50);
+            double sumAfterDiscont = sum - ((sum * 5) / 100);
+
+            Console.WriteLine($"Your total is {sumAfterDiscont:F2}lv");
+        }
+    }
+}
